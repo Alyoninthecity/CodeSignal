ceadd9067f5f36e926ef11bf451aa9b18ff9e852
Alyoninthecity <53126736+Alyoninthecity@users.noreply.github.com>
2021-11-23 12:57:12 +0100

    Create 12_SortByHeight.py

---
+++ Arcade Universe/Intro/12_SortByHeight.py
@@ -0,0 +1,36 @@
+"""
+Some people are standing in a row in a park. There are trees between them which cannot be moved. Your task is to rearrange the people by their heights in a non-descending order without moving the trees. People can be very tall!
+
+Example
+
+For a = [-1, 150, 190, 170, -1, -1, 160, 180], the output should be
+solution(a) = [-1, 150, 160, 170, -1, -1, 180, 190].
+
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[input] array.integer a
+
+If a[i] = -1, then the ith position is occupied by a tree. Otherwise a[i] is the height of a person standing in the ith position.
+
+Guaranteed constraints:
+1 ≤ a.length ≤ 1000,
+-1 ≤ a[i] ≤ 1000.
+
+[output] array.integer
+
+Sorted array a with all the trees untouched.
+"""
+
+
+def solution(a):
+    listaMem = []
+    for i in range(0, len(a)):
+        if a[i] == -1:
+            listaMem.insert(0, i)
+    a.sort()
+    for i in listaMem:
+        a.pop(0)
+        a.insert(i, -1)
+    return a
