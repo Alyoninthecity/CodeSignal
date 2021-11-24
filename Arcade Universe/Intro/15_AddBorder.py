78e0340b6bce3fb6b0a7e712051e1d59ee98a7f3
Alyoninthecity <53126736+Alyoninthecity@users.noreply.github.com>
2021-11-24 14:49:30 +0100

    Create 15_AddBorder.py

---
+++ Arcade Universe/Intro/15_AddBorder.py
@@ -0,0 +1,40 @@
+"""
+Given a rectangular matrix of characters, add a border of asterisks(*) to it.
+
+Example
+
+For
+
+picture = ["abc",
+           "ded"]
+the output should be
+
+solution(picture) = ["*****",
+                      "*abc*",
+                      "*ded*",
+                      "*****"]
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[input] array.string picture
+
+A non-empty array of non-empty equal-length strings.
+
+Guaranteed constraints:
+1 ≤ picture.length ≤ 100,
+1 ≤ picture[i].length ≤ 100.
+
+[output] array.string
+
+The same matrix of characters, framed with a border of asterisks of width 1.
+"""
+
+
+def solution(picture):
+    x = []
+    x.append("*" * (len(picture[0]) + 2))
+    for i in picture:
+        x.append("*" + i + "*")
+    x.append("*" * (len(picture[0]) + 2))
+    return x
